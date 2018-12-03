7f024262b9347d21c70030877a6ddf3a16b28316
Ishwor <ishworkhatri26@yahoo.com>
2018-12-03 08:00:49 +0545

    Learning Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
+  // selector: '[app-server]',
+  // selector: '.app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  //styleUrls: ['./server.component.css']
+  styles : [`
+    h1 {
+      color: blue;
+      border: 1px solid blue;
+      display: inline;
+      padding: 1rem;
+    }
+    .btn {
+      background-color: black;
+      color: white;
+      padding: 0.5rem 1rem;
+    }
+  `]
 })
 
-export class ServerComponent {
+export class ServerComponent implements OnInit{
+
+  allowNewServer = false;
+
+  serverId: number = 10;
+  serverStatus: string = "Offline !";
+  serverName: string = "LOCAL SERVER";
+
+  //method
+  myMethod(){
+    return this.serverName;
+  }
+
+  //Constructor
+  constructor(){
+    setTimeout(() => { this.allowNewServer = true; } ,10000);
+  }
+
+  ngOnInit(){}
 
 }
